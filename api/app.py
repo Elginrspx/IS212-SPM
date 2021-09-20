e4a49dd13ac378211f002a809cf722c39ef73df5
@edena.teo <edena.teo@biomarking.com>
2021-09-20 20:58:28 +0800

    put api tgt
    ported student and their completed courses api over
    
    added get registrations

--- api/app.py
+++ api/app.py
@@ -71,42 +71,6 @@ class Class(db.Model):
         "regPeriod": self.regPeriod
         }
 
-# class Registration(db.Model):
-#     __tablename__ = 'registrations'
-
-#     regStudentID = db.column(db.Integer, db.ForeignKey('classes.clsCourseID', ondelete="CASCADE", onupdate="CASCADE"), primary_key=True)
-#     clsCourseID = db.Column(db.String(30), db.ForeignKey('classes.clsCourseID', ondelete="CASCADE", onupdate="CASCADE"), primary_key=True)
-#     classID = db.Column(db.Integer,db.ForeignKey('classes.classID', ondelete="CASCADE", onupdate="CASCADE"), primary_key=True)
-#     clsTrainer = db.Column(db.String(30), nullable=False)
-#     clsStartTime = db.Column(db.String(30), nullable = False)
-#     clsEndTime = db.Column(db.String(30), nullable=False)
-#     clsLimit = db.Column(db.Integer, nullable=False)
-#     regPeriod = db.Column(db.String(30), nullable=False)
-#     # user = relationship('User', backref='child')
-#     classRegistration = db.relationship(
-#     'Course', primaryjoin='Class.clsCourseID == Course.courseID', backref='classes')
-
-#     def __init__(self, clsCourseID, classID, clsTrainer, clsStartTime, clsEndTime, clsLimit, regPeriod):
-#         self.clsCourseID = clsCourseID
-#         self.classID = classID
-#         self.clsTrainer = clsTrainer
-#         self.clsStartTime = clsStartTime
-#         self.clsEndTime = clsEndTime
-#         self.clsLimit = clsLimit
-#         self.regPeriod = regPeriod
-        
-
-#     def json(self):
-#         return {
-#         "clsCourseID": self.clsCourseID, 
-#         "classID": self.classID, 
-#         "clsTrainer": self.clsTrainer, 
-#         "clsStartTime": self.clsStartTime, 
-#         "clsEndTime": self.clsEndTime,
-#         "clsLimit": self.clsLimit,
-#         "regPeriod": self.regPeriod
-#         }
-
 
 class Prerequisite(db.Model):
     __tablename__ = 'prerequisites'
@@ -125,6 +89,76 @@ class Prerequisite(db.Model):
         return {
             "prereqCourseID" : self.prereqCourseID,
             "prereqName" : self.prereqName
+        }
+class Student(db.Model):
+    __tablename__ = 'students'
+
+    studentID = db.Column(db.Integer, primary_key=True)
+    studentName = db.Column(db.String(30), nullable=False)
+    sPosition = db.Column(db.String(30), nullable = False)
+
+
+    def __init__(self, studentID, studentName, sPosition):
+        self.studentID = studentID
+        self.studentName = studentName
+        self.sPosition = sPosition
+
+
+    def json(self):
+        return {
+            "studentID" : self.studentID,
+            "studentName": self.studentName,
+            "sPosition" : self.sPosition,
+        }
+
+class Completed(db.Model):
+    __tablename__ = 'completedCourses'
+
+    ccStudentID = db.Column(db.Integer, db.ForeignKey('students.studentID', ondelete="CASCADE", onupdate="CASCADE"), primary_key=True)
+    completedCName = db.Column(db.String(30), primary_key=True)
+
+    # user = relationship('User', backref='child')
+    studentCompleted = db.relationship(
+    'Student', primaryjoin='Student.studentID == Completed.ccStudentID', backref='completedCourses')
+
+    def __init__(self, ccStudentID, completedCName):
+        self.ccStudentID = ccStudentID
+        self.completedCName = completedCName
+    
+    def json(self):
+        return {
+        "ccStudentID": self.ccStudentID, 
+        "completedCName": self.completedCName
+        }
+
+class Registration(db.Model):
+    __tablename__ = 'registrations'
+
+    regStudentID = db.Column(db.Integer, db.ForeignKey('students.studentID', ondelete="CASCADE", onupdate="CASCADE"), primary_key=True)
+    regCourseID = db.Column(db.String(30), db.ForeignKey('classes.clsCourseID', ondelete="CASCADE", onupdate="CASCADE"), primary_key=True)
+    regClassID = db.Column(db.Integer,db.ForeignKey('classes.classID', ondelete="CASCADE", onupdate="CASCADE"), primary_key=True)
+    regStatus = db.Column(db.String(30), nullable=False)
+    # user = relationship('User', backref='child')
+    classRegistration1 = db.relationship(
+    'Class', primaryjoin='Class.clsCourseID == Registration.regCourseID', backref='registrations1')
+    classRegistration2 = db.relationship(
+    'Class', primaryjoin='Class.classID == Registration.regClassID', backref='registrations2')
+    studentRegistration = db.relationship(
+    'Student', primaryjoin='Student.studentID == Registration.regStudentID', backref='registrations3')
+
+    def __init__(self, regStudentID, regCourseID, regClassID, regStatus):
+        self.regStudentID = regStudentID
+        self.regCourseID = regCourseID
+        self.regClassID = regClassID
+        self.regStatus = regStatus
+        
+
+    def json(self):
+        return {
+        "regStudentID": self.regStudentID, 
+        "regCourseID": self.regCourseID, 
+        "regClassID": self.regClassID, 
+        "regStatus": self.regStatus
         }
 
 
@@ -214,7 +248,7 @@ def get_course_classes(courseID):
     except Exception as e:
         return jsonify({"message": "Class is not found." + str(e)}), 404
 
-# GET Class Details by Course-Class (haven't tested)
+# GET Class Details by Course-Class 
 @app.route("/classes/<string:courseID>/<string:classID>")
 def get_class_details(courseID, classID):
     try: 
@@ -251,6 +285,81 @@ def get_course_prereq(prereqCourseID):
             {
                 "code": 404,
                 "message": "There are no courses." + str(e)
+            }
+        ), 404
+
+
+# GET all completed Courses by studentID 
+@app.route("/completed/<string:ccStudentID>")
+def get_students_completed_courses(ccStudentID):
+    print(ccStudentID)
+    try:
+        courseList = Completed.query.filter_by(ccStudentID=ccStudentID).all()
+        
+        if len(courseList):
+            print(courseList)
+            print("meep")
+            return jsonify(
+                {
+                    "code": 200,
+                    "data": {
+                        "courses": [course.json() for course in courseList]
+                    }
+                }
+            )
+    except Exception as e:
+        return jsonify(
+            {
+                "code": 404,
+                "message": "There are no courses." + str(e)
+            }
+        ), 404
+
+# GET student info by studentName
+@app.route("/student/<string:studentID>")
+def get_student(studentID):
+    print(studentID)
+    try:
+        student = Student.query.filter_by(studentID=studentID).first()
+        
+        if (student):
+            print(student)
+            print("meep")
+            return jsonify(
+                {
+                    "code": 200,
+                    "data": {
+                        "courses": student.json()
+                    }
+                }
+            )
+    except Exception as e:
+        return jsonify(
+            {
+                "code": 404,
+                "message": "There are no courses." + str(e)
+            }
+        ), 404
+
+# GET student registrations by course+class
+@app.route("/registration/<string:courseID>/<string:classID>")
+def get_student_registration(courseID, classID):
+    try:
+        registrationn = Registration.query.filter_by(regCourseID=courseID, regClassID = classID).all()
+        if (registrationn):
+            return jsonify(
+                {
+                    "code": 200,
+                    "data": {
+                        "registrations": [registration.json() for registration in registrationn]
+                    }
+                }
+            )
+    except Exception as e:
+        return jsonify(
+            {
+                "code": 404,
+                "message": "There are no student registrations." + str(e)
             }
         ), 404
 
