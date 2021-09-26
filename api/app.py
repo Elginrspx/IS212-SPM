d1bee4531fb87118dce67bac6e8b6edb4d17fcd5
@edena.teo <edena.teo@biomarking.com>
2021-09-26 23:00:16 +0800

    Update app.py
    Fixed the course name duplication error

--- api/app.py
+++ api/app.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 from os import environ
 import json
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:@localhost:3306/systemdb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/systemdb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -495,7 +495,7 @@ def all_reg():
                     courseData["courseName"] = courseName
                     courseData["regCourseID"] = each[0]
                     courseList.append(courseData)
-                    courses.append(each[0])
+                    courses.append(courseName)
                     print(courseList)
                     # data['assignments']= each.json()
                     # print(data)
