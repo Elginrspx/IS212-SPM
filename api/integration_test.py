50f69a27ac92a9d84cf5f1a65e5d702aee5305c5
@edena.teo <edena.teo@biomarking.com>
2021-10-03 18:52:12 +0800

    Created TDD starter pack
    - included examples in example.py (credits to Chris)
    - included set up and tear down for testing app

---
+++ api/integration_test.py
@@ -0,0 +1,24 @@
+import unittest
+import flask_testing
+import json
+from app import app, db, Course, Class, Prerequisite, Student, Completed, Registration
+
+
+class TestApp(flask_testing.TestCase):
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite://"
+    app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {}
+    app.config['TESTING'] = True
+
+    def create_app(self):
+        return app
+
+    def setUp(self):
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+
+if __name__ == '__main__':
+    unittest.main()
