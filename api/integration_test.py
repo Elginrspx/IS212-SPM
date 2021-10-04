7b92d15d58b9f7f1d8fcb37a07533928ec608f81
Joel19997 <joelseah97@gmail.com>
2021-10-04 11:21:18 +0800

    Testing "Test2" branch
    Added comment to test if branching works

--- api/integration_test.py
+++ api/integration_test.py
@@ -8,6 +8,7 @@ class TestApp(flask_testing.TestCase):
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite://"
     app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {}
     app.config['TESTING'] = True
+#Country roads take me home
 
     def create_app(self):
         return app
