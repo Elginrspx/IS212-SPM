4589bb1823b4023a617de54679b19a9beb39042e
tyh2312 <31516640+tyh2312@users.noreply.github.com>
2021-10-13 17:08:24 +0800

    Create TDD for classes

--- api/testApp.py
+++ api/testApp.py
@@ -5,7 +5,7 @@ from app import app, db, Course, Class, Prerequisite, Student, Completed, Regist
 
 
 class TestApp(flask_testing.TestCase):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/testdb'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/testdb'
     app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {}
     app.config['TESTING'] = True
 #Country roads take me home
@@ -48,6 +48,70 @@ class TestCreateRegistration(TestApp):
 
     # def test_create_registration_prereqs_not_satisfied(self):
     # def test_create_registration_already_registered(self):
+
+# LMS Classes - YH 
+class TestCreateClasses(TestApp):
+    def test_create_class(self):
+        c1 = Class(13, 2, "Fong Heng Heng", "2 October 2021", "3 October 2023", 25, "3 Sept, 2021 to 15 Sept, 2021")
+        db.session.add(c1)
+        db.session.commit()
+
+        request_body = {
+            'clsCourseID': c1.id,
+            'classID': c1.id,
+            'clsTrainer': 'Fong Heng Heng',
+            'clsStartTime': '2 October 2021',
+            'clsEndTime': '3 October 2023',
+            'clsLimit': 25,
+            'regPeriod': '3 Sept, 2021 to 15 Sept, 2021'
+        }
+
+        response = self.client.post("/classes",
+                                            data=json.dumps(request_body),
+                                            content_type='application/json')
+        
+        self.assertEqual(response.json, {
+            'clsCourseID': c1.id,
+            'classID': c1.id,
+            'clsTrainer': 'Fong Heng Heng',
+            'clsStartTime': '2 October 2021',
+            'clsEndTime': '3 October 2023',
+            'clsLimit': 25,
+            'regPeriod': '3 Sept, 2021 to 15 Sept, 2021'
+        })
+    
+    def test_get_class(self):
+        response = self.client.get("/classes")
+        self.assertEqual(response.json['code'], 200)
+
+    def test_get_classID(self):
+        response = self.client.get("/classes/12")
+        self.assertEqual(response.json['code'], 200)
+
+    def test_create_class_invalidDate(self):
+        c1 = Class(13, 2, "Fong Heng Heng", "2 September 2021", "3 October 2023", 25, "3 Sept, 2021 to 15 Sept, 2021")
+        db.session.add(c1)
+        db.session.commit()
+
+        request_body = {
+            'clsCourseID': c1.id,
+            'classID': c1.id,
+            'clsTrainer': 'Fong Heng Heng',
+            'clsStartTime': '2 September 2021',
+            'clsEndTime': '3 October 2023',
+            'clsLimit': 25,
+            'regPeriod': '3 Sept, 2021 to 15 Sept, 2021'
+        }
+
+        response = self.client.get("/classes",
+                                    data=json.dumps(request_body),
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, 500)
+        self.assertEqual(response.json, {
+            'message': 'Class start date should be later than registration period.'
+        })
+
+
 
 #testing if it works
 
