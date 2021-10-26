df4a254973953c2a6bfb5d93025dcfe2abe86a67
@edena.teo <edena.teo@biomarking.com>
2021-10-26 21:55:20 +0800

    update question.py
    - added isMultiple to variables
    - created function to compute score in question.py

--- api/question.py
+++ api/question.py
@@ -1,39 +1,20 @@
 from main import db
-<<<<<<< Updated upstream
-=======
 from sqlalchemy import *
 from sqlalchemy.orm import relationship
 from sqlalchemy import ForeignKeyConstraint
 from section import Section
->>>>>>> Stashed changes
 
 
 class Question(db.Model):
     __tablename__ = 'questions'
 
-<<<<<<< Updated upstream
-    secCourseID = db.Column(db.Integer, primary_key=True)
-    secClassID = db.Column(db.String(30), nullable=False)
-    sectionID = db.Column(db.String(30))
-    question = db.Column(db.String(255))
-    answer = db.Column(db.String(255))
-    other = db.Column(db.String(255))
-
-
-    def __init__(self, secCourseID, secClassID, sectionID, question, answer, other):
-        self.secCourseID = secCourseID
-        self.secClassID = secClassID
-        self.sectionID = sectionID
-        self.question = question
-        self.answer = answer
-        self.other = other
-=======
     qnCourseID = db.Column(db.String(30), primary_key=True)
     qnClassID = db.Column(db.Integer, nullable=False, primary_key=True)
     qnSectionID = db.Column(db.String(30), nullable=False, primary_key=True)
     question = db.Column(db.String(255), primary_key=True)
     answer = db.Column(db.String(255), nullable=False)
     choices = db.Column(db.String(255), nullable=False)
+    isMultiple = db.Column(db.String(5), nullable=False)
 
     __table_args__ = (ForeignKeyConstraint([qnCourseID, qnClassID, qnSectionID],
                     [Section.secCourseID, Section.secClassID, Section.sectionID]),
@@ -41,14 +22,14 @@ class Question(db.Model):
     sectionQuiz = db.relationship(
     'Section', primaryjoin='and_(Question.qnClassID == Section.secClassID, Section.secCourseID == Question.qnCourseID, Section.sectionID == Question.qnSectionID)', backref='questions')
 
-    def __init__(self, qnCourseID, qnClassID, qnSectionID, question, answer, choices):
+    def __init__(self, qnCourseID, qnClassID, qnSectionID, question, answer, choices, isMultiple):
         self.qnCourseID = qnCourseID
         self.qnClassID = qnClassID
         self.qnSectionID = qnSectionID
         self.question = question
         self.answer = answer
         self.choices = choices
->>>>>>> Stashed changes
+        self.isMultiple = isMultiple
 
 
     def json(self):
@@ -58,28 +39,36 @@ class Question(db.Model):
             "qnSectionID" : self.qnSectionID,
             "question": self.question,
             "answer": self.answer,
-<<<<<<< Updated upstream
-            "other": self.other
-        }
-=======
-            "choices": self.choices
+            "choices": self.choices,
+            "isMultiple": self.isMultiple
         }
 
     def get_questions(qnCourseID, qnClassID, qnSectionID):
-        print("hiiii")
-        
         try:
-            questions = db.session.query(Question.question, Question.answer, Question.choices).filter(qnCourseID==Question.qnCourseID, qnClassID == Question.qnClassID, qnSectionID == Question.qnSectionID).all()
+            questions = db.session.query(Question.question, Question.choices, Question.isMultiple).filter(qnCourseID==Question.qnCourseID, qnClassID == Question.qnClassID, qnSectionID == Question.qnSectionID).all()
             if questions:
                 questionList = []
                 for questionSet in questions:
                     questionData = {}
                     questionData["question"] = questionSet[0]
-                    questionData["answer"] = questionSet[1]
-                    questionData["choices"] = questionSet[2]
+                    questionData["choices"] = questionSet[1]
+                    questionData["isMultiple"] = questionSet[2]
                     questionList.append(questionData)
                 return 200, questionList
         except Exception as e:
             return 404, "Questions not available" + str(e)
 
->>>>>>> Stashed changes
+
+    def compute_score(data, qnCourseID, qnClassID, qnSectionID):
+        maxscore = len(data)
+        score = 0
+        answerKey = db.session.query(Question.question, Question.answer).filter(qnCourseID==Question.qnCourseID, qnClassID == Question.qnClassID, qnSectionID == Question.qnSectionID).all()
+        for qn1 in data:
+            for qn2 in answerKey:
+                if qn1["qn"] == qn2[0]:
+                    if qn1["ans"] == qn2[1]:
+                        score += 1
+                        break
+        score_percentage = score/maxscore
+        return score_percentage
+
