0ba1be57749d3902833e07c21852a83920685ea7
@edena.teo <edena.teo@biomarking.com>
2021-10-26 22:17:24 +0800

    Update createDatabase.py
    -added test data for score

--- api/createDatabase.py
+++ api/createDatabase.py
@@ -8,6 +8,7 @@ from registration import Registration
 from student import Student
 from section import Section
 from question import Question
+from studentScore import Score
 
 
 db.drop_all()
@@ -19,9 +20,12 @@ dummyClass = Class(1,3, "Lim Ah Hock", "12-Sept-2021", "14-Sept-2023", 35, "9 Oc
 dummyUserReg = Registration("1","1","3","enrolled")
 dummyPrerequisite = Prerequisite(2, "3D Printing Software v1.0")
 dummySection = Section(1,3,"Chapter2", 3)
-dummyQN1 = Question(1,3,"Chapter2", "What is the answer", "the answer", "Answer, theanswer, the answer, ans")
-dummyQN2 = Question(1,3,"Chapter2", "What is the answer now", "ans", "Answers, theanswers, the answers, ans")
-dummyQN3 = Question(1,3,"Chapter2", "What is the answer teehee", "Answer", "Answer, theanswer, the answer, ans")
+dummyQN1 = Question(1,3,"Chapter2", "What is the answer", "the answer", "Answer, theanswer, the answer, ans", "true")
+dummyQN2 = Question(1,3,"Chapter2", "What is the answer now", "ans", "Answers, theanswers, the answers, ans", "true")
+dummyQN3 = Question(1,3,"Chapter2", "What is the answer teehee", "Answer", "Answer, theanswer, the answer, ans", "false")
+dummyScore1 = Score(1,1,3,"Chapter2", 80)
+dummyScore2 = Score(2,1,3,"Chapter2", 70)
+dummyScore3 = Score(3,1,3,"Chapter2", 80)
 db.session.add(dummyCourse)
 db.session.add(dummyCourse2)
 db.session.add(dummyClass)
@@ -31,6 +35,9 @@ db.session.add(dummySection)
 db.session.add(dummyQN3)
 db.session.add(dummyQN1)
 db.session.add(dummyQN2)
+db.session.add(dummyScore1)
+db.session.add(dummyScore2)
+db.session.add(dummyScore3)
 db.session.commit()
 
 
