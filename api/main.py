4c39a44d0514498230dc9a7193180108c4ef1b56
Joel19997 <joelseah97@gmail.com>
2021-11-01 15:28:42 +0800

    Updating studentSelectQuiz.html
    saving progress

--- api/main.py
+++ api/main.py
@@ -8,7 +8,7 @@ from os import environ
 import json
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/systemdb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/systemdb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
